18e9e27a6ed0af167518165b9a658f70698506ea
vladislvk <vladislavkim566@gmail.com>
2024-09-10 20:47:19 +0500

    feat:blank

---
+++ dota-companion/dota_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  dota_companionApp.swift
+//  dota-companion
+//
+//  Created by vladislav kim on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct dota_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
